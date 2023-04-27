6e53aa83f6a0e7b721f09c496a53d513aabe2371
edancy <elizabeth.dancy@sheridancollege.ca>
2023-04-27 09:46:18 -0400

    new code

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ *
+ * @author dancye
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
