150c13d1be0a458c9f598cfc70fedf38329edc17
edancy <elizabeth.dancy@sheridancollege.ca>
2023-04-27 09:46:59 -0400

    new code

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -10,7 +10,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      System.out.println("some small change");
     }
     
 }
